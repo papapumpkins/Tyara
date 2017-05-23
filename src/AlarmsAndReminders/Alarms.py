1e81468f94b36a3dec88133efdf0d41ed07c367e
neelotpalnag <neelotpalnag@gmail.com>
2017-05-23 14:25:45 +0530

    Commit performed 05/22/2017. Compilation = true. API keys Valid.

--- src/AlarmsAndReminders/Alarms.py
+++
@@ -1,7 +0,0 @@
-def setAlarms(dialog):
-
-    alarm_time_dialog = extract_alarm_time(dialog)
-    return "An Alarm has been set for " + alarm_time_dialog
-
-def extract_alarm_time(dialog):
-    return "0700 hours"
